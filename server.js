77c5b5966eac6dec773f8458f6ec4371d9834ab2
jameshhe <jameshuang@smu.edu>
2021-08-20 08:37:32 -0500

    changed file structure and attempting to deploy the entire app to heroku only

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use("/", express.static(path.join(__dirname, "/client/build")));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
@@ -36,6 +38,10 @@ mongoose
   .catch((err) => console.log(err));
 
 routes(app);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
