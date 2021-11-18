4423ce5524c3afbd47ae2ee8ba602d2d71ca1d47
James Huang <hhe990219@hotmail.com>
2021-11-18 11:28:57 -0600

    setup(test): added nav.test and app.test

--- client/src/tests/Nav.test.js
+++ client/src/tests/Nav.test.js
@@ -1,22 +1,23 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Navigation from "../components/navigation";
 
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
+import { SET_CURRENT_USER } from "../actions/types";
 
-describe("Test Navbar with mock store", () => {
+describe("Test navbar under multiple scenarios", () => {
   const initialState = { isAuthenticated: false, user: {}, loading: false };
   const mockStore = configureStore();
   let store, wrapper;
 
-  it("Contains Services", () => {
-    store = mockStore(initialState);
-    const { getByText } = render(
-      <Provider store={store}>
-        <Navigation />
-      </Provider>
-    );
-
-    expect(getByText("Services")).toBeInTheDocument();
+  store = mockStore(initialState);
+  render(
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+  it("should only contain Services when user is not logged in", () => {
+    expect(screen.getByText("Services")).toBeInTheDocument();
+    expect(screen.queryByText("My Profile")).toBeNull();
   });
 });
